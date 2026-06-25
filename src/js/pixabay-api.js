15ac9bf3b4bf6ee61eb93f557feed355103681e2
danavykhovanets-hub <dana.vykhovanets@gmail.com>
2026-06-25 19:48:21 -0400

    completion of h/w

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '56452748-acb3c6ecf21893b07db51bc11';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  }).then(response => response.data);
+}
---
+++ src/main.js
@@ -0,0 +1,38 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const query = event.target.elements['search-text'].value.trim();
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      hideLoader();
+
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message: 'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      hideLoader();
+      iziToast.error({
+        message: `Something went wrong: ${error.message}`,
+        position: 'topRight',
+      });
+    });
+});
